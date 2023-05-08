a913f585b6ec901f305679847c9903c25a60a074
leianzhan <leianzhan@gmail.com>
2023-05-09 02:29:01 +0800

    0509 sqldatabase修改

--- Cats_Villa/Program.cs
+++ Cats_Villa/Program.cs
@@ -16,7 +16,7 @@ namespace Cats_Villa
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new FormMain());
+			Application.Run(new FormLogin());
 		}
 	}
 }
